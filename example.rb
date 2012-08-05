5948c47dd7197f4797d9f3b3cab1887e4b9f0e93
Nuwan Sameera <nuwan28@gmail.com>
2012-08-05 23:59:13 +0530

    Initial commit for RDCSS (Rails Documented CSS)

--- example.rb
+++
@@ -1,6 +0,0 @@
-Rdcss::Engine.routes.draw do
-  # Define root path, example: http://test.local:3000/dcss/ 
-  root :to => "home#index" 
-  # Define styleguide paths, exmaple: http://test.local:3000/dcss/styleguides
-  resources :styleguides 
-end
